df4b46486ec7024d5d5b8f6c7d7495ac1e65e78a
ligulfzhou53 <ligulfzhou53@gmail.com>
2016-02-16 18:51:52 +0800

    ui segment controll

---
+++ Swift/UISegmentControll/UISegmentControll/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  UISegmentControll
+//
+//  Created by ligulfzhou on 2/16/16.
+//  Copyright © 2016 ligulfzhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var segments: UISegmentedControl!
+    var items: [String] = ["hello", "ligulf", "zhou", "!!!"]
+    
+    //item 也可以为图片
+//    var item = NSArray(objects: "hello", "ligulf", "zhou", UIImage(named: "imagename"))
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        segments = UISegmentedControl(items: items)
+//        segments.frame = CGRectMake(0, 0, 300, 50)
+        segments.center = view.center
+        
+//        segments.momentary = true 
+        view.addSubview(segments)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
