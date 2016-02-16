577eb8fc3c21bd4ec82ffe66628c7c270c369bbf
ligulfzhou53 <ligulfzhou53@gmail.com>
2016-02-16 18:14:47 +0800

    ui slider

---
+++ Swift/UISlider/UISlider/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  UISlider
+//
+//  Created by ligulfzhou on 2/16/16.
+//  Copyright © 2016 ligulfzhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var slider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        slider = UISlider(frame: view.frame)
+        slider.center = view.center
+        
+        slider.minimumValue = 0
+        slider.maximumValue = 100
+        slider.addTarget(self, action: "sliderTarget:", forControlEvents: .ValueChanged)
+        
+        view.addSubview(slider)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    //MARK: slider target func
+    func sliderTarget(sender: UISlider){
+        NSLog("\(sender.value)")
+    }
+}
+
