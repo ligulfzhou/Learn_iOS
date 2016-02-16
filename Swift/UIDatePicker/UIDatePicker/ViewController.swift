a039d111ebbad8399957e798f66e79e020be437a
ligulfzhou53 <ligulfzhou53@gmail.com>
2016-02-16 18:05:05 +0800

    ui date picker

---
+++ Swift/UIDatePicker/UIDatePicker/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  UIDatePicker
+//
+//  Created by ligulfzhou on 2/16/16.
+//  Copyright © 2016 ligulfzhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var datePicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        datePicker = UIDatePicker(frame: view.frame)
+        datePicker.center = view.center
+        datePicker.addTarget(self, action: "datePickerChange:", forControlEvents: .ValueChanged)
+        
+        // 限制时间间隔
+        let oneYearTime:NSTimeInterval = 365 * 24 * 60 * 60
+        let todayDate = NSDate()
+        let oneYearFromToday = todayDate.dateByAddingTimeInterval(oneYearTime)
+        let twoYearsFromToday = todayDate.dateByAddingTimeInterval(2 * oneYearTime)
+        datePicker.minimumDate = oneYearFromToday
+        datePicker.maximumDate = twoYearsFromToday
+        
+        // 时间格式
+        datePicker.datePickerMode = .Date
+        
+        view.addSubview(datePicker)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func datePickerChange(sender: UIDatePicker){
+        NSLog("\(sender.date)")
+    }
+
+}
+
